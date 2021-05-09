ba6703b16205626d124974d2d1704f2ff4921432
justinbrick <justinbrick1@gmail.com>
2021-05-09 11:48:54 -0400

    feat: Documentation fix: Remove useless dependency

--- src/main/java/core/Vector2.java
+++ src/main/java/core/Vector2.java
@@ -13,18 +13,21 @@ public class Vector2
     private float x;
     private float y;
 
+    /* Normal Constructor */
     public Vector2(float x, float y)
     {
         this.x = x;
         this.y = y;
     }
 
+    /* Copy Constructor */
     public Vector2(Vector2 vector)
     {
         setX(vector.getX());
         setY(vector.getY());
     }
 
+    /* Setters and Getters */
     public float getX()
     {
         return x;
@@ -45,24 +48,42 @@ public class Vector2
         this.y = y;
     }
 
+    /**
+     * Subtracts from the current vector the values of the one given.
+     * @param vec the vector we are subtracting this one with.
+     */
     public void subtract(Vector2 vec)
     {
         x -= vec.getX();
         y -= vec.getY();
     }
 
+    /**
+     * Adds to the current vector the values of the one given.
+     * @param vec the vector we are adding this one with.
+     */
     public void add(Vector2 vec)
     {
         x += vec.getX();
         y += vec.getY();
     }
 
+    /**
+     * Sets this vector's X to this and returns an instance of the same vector.
+     * @param x What will the X be?
+     * @return Vector with this X value.
+     */
     public Vector2 withX(float x)
     {
         this.x = x;
         return new Vector2(this);
     }
 
+    /**
+     * Sets this vector's Y to this and returns an instance of the same vector.
+     * @param y What will the Y be?
+     * @return Vector with this Y value.
+     */
     public Vector2 withY(float y)
     {
         this.y = y;
